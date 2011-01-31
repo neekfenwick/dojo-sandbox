aa6c81dd57248d73432b11570e67f5f5d109a450
Ferdinand <wuhi@nod.at>
2011-01-31 10:22:09 +0100

    moved editors into seperate widget

--- app/public/lib/sandbox/Frontend.js
+++ app/public/lib/sandbox/Frontend.js
@@ -10,14 +10,14 @@ dojo.require("dijit.form.CheckBox");
 dojo.require("dojox.data.JsonRestStore");
 dojo.require("dijit.layout.TabContainer");
 dojo.require("dijit.form.DropDownButton");
-dojo.require("dojox.html.format");
+
+dojo.require("sandbox.frontend.EditorTab");
 
 dojo.declare("sandbox.Frontend", [dijit.layout.ContentPane, dijit._Templated], {
 
 	widgetsInTemplate: true,
 	templateString: dojo.cache("sandbox", "templates/Frontend.html"),
 
-	_basePath: "/lib/codemirror/",
 	_editors: [],
 	_layersCBs: [],
 	// array of checkboxes for layer selection
@@ -42,62 +42,10 @@ dojo.declare("sandbox.Frontend", [dijit.layout.ContentPane, dijit._Templated], {
 	nlsString: {
 		STR_NOT_IMPLEMENTED: 'Not yet implemented'
 	},
-	_editorMixin: {
-		"initialized": false,
-		"baseNode": null,
-		"widget": null,
-		"getValue": function () {
-			return this.widget.getCode();
-		},
-		"setValue": function (value) {	
-			if(!value){
-				return;
-			}
-			if(this.initialized == false){
-				this._valueToInit = value;
-			}else{
-				this.widget.setCode(value);
-			}
-		},
-		"onInit": function(){
-			this.setValue(this._valueToInit);
-		},
-		"autoFormat": function(){
-			alert("not yet implemented");
-		}		
-	},
-	constructor: function () {
-		this._editors = [dojo.delegate(this._editorMixin, {
-			"id": "javascript",
-			"initialized": false,
-			"containerNode": "centerLeftPane",
-			"syntax": "js",
-			"defaultValue": "/* js code here */",
-			"autoFormat": function(){
-				this.setValue( js_beautify( this.getValue() ) );
-			}
-		}),
-		dojo.delegate(this._editorMixin, {
-			"id": "html",
-			"containerNode": "centerTopRightPane",
-			"syntax": "html",
-			"defaultValue": "<!-- html code here -->",
-			"autoFormat": function(){
-				this.setValue( dojox.html.format.prettyPrint(dojox.html.entities.decode( this.getValue() )) );
-			}
-		}),
-		dojo.delegate(this._editorMixin, {
-			"id": "css",
-			"containerNode": "centerBottomRightPane",
-			"syntax": "css",
-			"defaultValue": "/* css code here */"
-		})];
-	},
-
+	
 	postCreate: function () {
 		this.inherited(arguments);
 
-		this.setupEditors();
 		this.fetchConfig();
 		this.djConfig.onKeyUp();
 		
@@ -269,7 +217,7 @@ dojo.declare("sandbox.Frontend", [dijit.layout.ContentPane, dijit._Templated], {
 									editor.setValue(response[responseField]);
 								};
 								var setValuePartial = dojo.partial(setValueFn, response);
-								dojo.forEach(this._editors, function(editor) {
+								dojo.forEach(this.editorTab._editors, function(editor) {
 									if (editor.id == 'html') {
 										this('content_html', editor);
 									} else if (editor.id == 'css') {
@@ -322,89 +270,10 @@ dojo.declare("sandbox.Frontend", [dijit.layout.ContentPane, dijit._Templated], {
 			b.placeAt(this.userInfoNode);
 		}
 	},
-
-
 	_updateBucketInfoNodes: function() {
 		dojo.attr(this.bucketNamespaceNode, 'innerHTML', this._bucketInfo.namespace);
 		dojo.attr(this.bucketIdNode, 'innerHTML', this._bucketInfo.id);
 		dojo.attr(this.bucketVersionNode, 'innerHTML', this._bucketInfo.version);
-	},
-
-	setupEditors: function () {
-		window["keyBindings"] = {
-			enter: "newline-and-indent",
-			tab: "reindent-selection",
-			ctrl_z: "undo",
-			ctrl_y: "redo",
-			ctrl_backspace: "undo-for-safari (which blocks ctrl-z)",
-			ctrl_bracket: "highlight-brackets",
-			ctrl_shift_bracket: "jump-to-matching-bracket"
-		};
-
-		dojo.forEach(this._editors, function (editor) {
-
-			editor.baseNode = dojo.create("textarea", {
-					"style": {
-						"height": "100%",
-						"width": "100%"
-					}
-				}, this[editor.containerNode].containerNode);
-			
-			var parserfile = "";
-			var stylesheet = "";
-			switch (editor.syntax) {
-			case "html":
-				parserfile = ["parsexml.js", "parsecss.js", "tokenizejavascript.js", "parsejavascript.js", "parsehtmlmixed.js"];
-				stylesheet = [this._basePath + "css/xmlcolors.css", this._basePath + "css/jscolors.css", this._basePath + "css/csscolors.css"];
-				break;
-
-			case "js":
-				parserfile = ["tokenizejavascript.js", "parsejavascript.js"];
-				stylesheet = this._basePath + "css/jscolors.css";
-				break;
-
-			case "css":
-				parserfile = "parsecss.js";
-				stylesheet = this._basePath + "css/csscolors.css";
-				break;
-			}
-
-			editor.widget = CodeMirror.fromTextArea(editor.baseNode, {
-				"height": "90%",
-				"width": "100%",
-				"content": editor.defaultValue,
-				"parserfile": parserfile,
-				"stylesheet": stylesheet,
-				"path": this._basePath + "js/",
-				"autoMatchParens": true,
-				"onLoad": dojo.hitch(editor, function(){
-					this.initialized = true;
-					this.onInit();
-				})
-			});
-
-			var toolbar = new dijit.Toolbar().placeAt(editor.widget.wrapping, "before");
-			var menu = new dijit.Menu({
-				style: "display: none;"
-			});
-
-			menu.addChild(new dijit.MenuItem({
-				label: "autoformat",
-				onClick: dojo.partial(function (scope, evt) {
-					scope.autoFormat();
-				}, editor)
-			}));
-			
-			dojo.forEach(editor.additionalMenuItems, function(menuItem){
-				menu.addChild(menuItem);
-			}, this);
-
-			toolbar.addChild(new dijit.form.DropDownButton({
-				"label": editor.id,
-				dropDown: menu
-			}));
-
-		}, this);
 	},
 	_selectTab: function(tab){
 		if(tab == this.designerWidget){
@@ -412,9 +281,7 @@ dojo.declare("sandbox.Frontend", [dijit.layout.ContentPane, dijit._Templated], {
 		}
 	},
 	_getEditorItem: function (id) {
-		return dojo.filter(this._editors, function (editor) {
-			return (editor.id == id) ? editor : undefined;
-		})[0];
+		return this.editorTab._getEditorItem(id);
 	},
 
 	getUserDetails: function (username) {
