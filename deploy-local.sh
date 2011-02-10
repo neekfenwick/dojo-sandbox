1aaa09a4507e9245f9465919f9e794ba68a25459
Ferdinand <wuhi@nod.at>
2011-02-10 12:17:07 +0100

    deploy without ssh upload

---
+++ deploy-local.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# deploy.sh
+# A helper script to rsync the right bits to Live
+
+if [[ ! -d app ]] ; then
+	echo I expect to be run from the top level dir containing the 'app' dir
+	exit 1
+fi
+
+# rsync to remote server, avoiding copying the
+#  configs which are platform specific.
+rsync --recursive --update --verbose --compress --exclude=app/application/configs app /var/www/sandbox
