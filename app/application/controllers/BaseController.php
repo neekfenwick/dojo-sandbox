b698c27992c78fb3929d7117550af62457a4589e
Nick Fenwick <neek@nickfenwick.com>
2011-06-18 15:28:29 +0700

    Trying to write to log file instead of error_log.

--- app/application/controllers/BaseController.php
+++ app/application/controllers/BaseController.php
@@ -5,6 +5,10 @@ abstract class BaseController extends Zend_Rest_Controller {
 
   public static function initLogger() {
     self::$logger = new Zend_Log();
+    #$writer = new Zend_Log_Writer_Stream('php://stderr');
+    #$writer = new Zend_Log_Writer_Syslog(array('facility' => 'sandbox'));
+    #$options = Zend_Controller_Front::getInstance()->getParam('bootstrap')->getOptions();
+    #error_log("option: " . $options->logfile->path);
     $writer = new Zend_Log_Writer_Stream('php://stderr');
     self::$logger->addWriter($writer);
   }
